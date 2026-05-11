dc32ed924ac01589f6c0d18d2149172b41ddb1c9
spscah <cah@stpaulsschool.org.uk>
2026-05-11 09:12:34 +0100

    end of term

--- exams.js
+++ exams.js
@@ -6,4 +6,7 @@ window.EXAMS = [
   { subject: "1CP2", paper: "Paper 2", month: 5, day: 19 },
   { subject: "FM",   paper: "Y544",    month: 6, day: 16 },
   { subject: "SM",   paper: "Y240/2",  month: 6, day: 18 },
+  { subject: "Last",   paper: "HT",  month: 5, day: 22 },
+  { subject: "Last",   paper: "T",  month: 7, day: 3 },
+
 ];
